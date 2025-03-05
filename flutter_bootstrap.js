6d4083e682fd4c78a69eadaf404961bdc80afde2
Cookie <88719371+Cookiezikse@users.noreply.github.com>
2025-03-05 16:16:46 +0100

    NEW : favicon

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1621675561"
+    serviceWorkerVersion: "3231784474"
   }
 });
